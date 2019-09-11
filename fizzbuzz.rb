dd0092c940636be9501d8fc770885ac0ec6cbc5c
Tonik Profik <izanami369@gmail.com>
2019-09-11 13:53:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,17 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
     puts"FizzBuzz"
+  end
   elsif number % 3 == 0
     puts"Fizz"
+  end
   elsif number % 5 == 0
     puts"Buzz"
+  end
+  
   else
     return nil
+  end
 end
 
 fizzbuzz(5)
